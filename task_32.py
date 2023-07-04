7c58e0346da7f5408f16488cda3e51860458bc00
Egor <egoriq43@gmail.com>
2023-07-04 22:47:14 +0700

    hw_lesson_6_done

---
+++ task_32.py
@@ -0,0 +1,9 @@
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+list_2 = []
+max = 10
+min = 6
+for i in range(len(list_1)):
+    if list_1[i] >= min and list_1[i] <= max:
+         list_2.append(i)
+print(list_2)
+
